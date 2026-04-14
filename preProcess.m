977d27d79c869af05bdda0ce83e394635554d57c
Addison Nally <an10-1999@hotmail.com>
2026-04-14 16:37:36 -0500

    Added Pre Processing Function

---
+++ preProcess.m
@@ -0,0 +1,23 @@
+function outFrame = preProcess(inFrame, targetSize)
+% Addison Nally
+% preProcess.m
+% Simple preprocessing for face detection
+
+    if nargin < 1 || isempty(inFrame)
+        error('preProcess requires one input frame.');
+    end
+
+    if nargin < 2
+        targetSize = [200 200];
+    end
+
+    if size(inFrame, 3) == 3
+        grayFrame = rgb2gray(inFrame);
+    else
+        grayFrame = inFrame;
+    end
+
+    eqFrame = histeq(grayFrame);
+    resizedFrame = imresize(eqFrame, targetSize);
+    outFrame = medfilt2(resizedFrame, [3 3]);
+end
