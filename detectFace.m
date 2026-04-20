44514a78ed0e457ce612ab655e95f0e96bdbd444
Joey Suliguin <jspsuliguin@gmail.com>
2026-04-20 12:08:53 -0500

    Enhance detectFace function with detailed comments and maintain detection parameters for improved face detection accuracy

--- detectFace.m
+++ detectFace.m
@@ -1,5 +1,12 @@
 function boundingBox = detectFace(frame)
-% detectFace.m
-    detector = vision.CascadeObjectDetector('FrontalFaceCART');
-    boundingBox = detector(frame);
+% detectFace.m - Detect multiple faces in an image
+
+detector = vision.CascadeObjectDetector('FrontalFaceCART');
+
+
+detector.MinSize = [30 30];      % Detect faces as small as 30x30 pixels
+detector.MergeThreshold = 4;     % Merge nearby detections for multiple faces
+
+% Detect all faces in the frame
+boundingBox = detector(frame);
 end
