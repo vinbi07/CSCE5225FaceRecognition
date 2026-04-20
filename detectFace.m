3d4e1cc493374e646465e9fdf3ff7c5cfa561976
Addison Nally <an10-1999@hotmail.com>
2026-04-20 15:58:05 -0500

    improved facial detection
    -added side face detection
    -detect faces up to a 15 degree tilt

--- detectFace.m
+++ detectFace.m
@@ -1,12 +1,52 @@
 function boundingBox = detectFace(frame)
-% detectFace.m - Detect multiple faces in an image
+% detectFace.m
+% Detect frontal faces, side faces, and slightly tilted faces
 
-detector = vision.CascadeObjectDetector('FrontalFaceCART');
+frontalDetector = vision.CascadeObjectDetector('FrontalFaceCART');
+profileDetector = vision.CascadeObjectDetector('ProfileFace');
 
+frontalDetector.MinSize = [30 30];
+frontalDetector.MergeThreshold = 4;
 
-detector.MinSize = [30 30];      % Detect faces as small as 30x30 pixels
-detector.MergeThreshold = 4;     % Merge nearby detections for multiple faces
+profileDetector.MinSize = [30 30];
+profileDetector.MergeThreshold = 4;
 
-% Detect all faces in the frame
-boundingBox = detector(frame);
+boundingBox = [];
+
+% Detect frontal faces
+frontBoxes = frontalDetector(frame);
+boundingBox = [boundingBox; frontBoxes];
+
+% Detect side/profile faces
+sideBoxes = profileDetector(frame);
+boundingBox = [boundingBox; sideBoxes];
+
+% Detect opposite side by flipping image
+flipFrame = fliplr(frame);
+flipBoxes = profileDetector(flipFrame);
+
+if ~isempty(flipBoxes)
+    imgWidth = size(frame, 2);
+    for i = 1:size(flipBoxes, 1)
+        flipBoxes(i,1) = imgWidth - flipBoxes(i,1) - flipBoxes(i,3);
+    end
+    boundingBox = [boundingBox; flipBoxes];
+end
+
+% Detect slight head tilt
+leftTilt = imrotate(frame, 15, 'crop');
+rightTilt = imrotate(frame, -15, 'crop');
+
+leftBoxes = frontalDetector(leftTilt);
+rightBoxes = frontalDetector(rightTilt);
+
+boundingBox = [boundingBox; leftBoxes; rightBoxes];
+
+% Remove overlapping boxes
+if ~isempty(boundingBox)
+    scores = ones(size(boundingBox,1),1);
+    boundingBox = selectStrongestBbox(boundingBox, scores, ...
+        'OverlapThreshold', 0.3);
+end
+
 end
